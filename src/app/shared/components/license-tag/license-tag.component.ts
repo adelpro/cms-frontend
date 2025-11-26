5b38f8430c3e208e758d0660492a066331925fd2
adelpro <adelpro@gmail.com>
2025-11-26 10:14:22 +0100

    feat: add proper licences ui/ux

--- src/app/shared/components/license-tag/license-tag.component.ts
+++ src/app/shared/components/license-tag/license-tag.component.ts
@@ -1,20 +1,50 @@
-import { Component, input } from '@angular/core';
-import { NzTagComponent } from 'ng-zorro-antd/tag';
-import { Licenses, LicensesColors } from '../../../core/enums/licenses.enum';
 import { NgClass } from '@angular/common';
+import { Component, HostListener, Input, OnInit, inject, input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { Licenses, LicensesColors } from '../../../core/enums/licenses.enum';
 
 @Component({
   selector: 'app-license-tag',
-  imports: [NzTagComponent, NgClass],
-  template: `
-    <nz-tag [nzColor]="nzColor" [ngClass]="{muted: muted()}">{{ license() }}</nz-tag>
-  `
+  standalone: true,
+  imports: [NgClass, NzTagModule, NzIconModule, NzPopoverModule, NzDrawerModule],
+  templateUrl: './license-tag.component.html',
+  styleUrls: ['./license-tag.component.less'],
 })
-export class LicenseTagComponent {
-  license = input.required<Licenses>();
+export class LicenseTagComponent implements OnInit {
+  @Input() license!: Licenses;
+
+  showPopover = false;
+  isMobileView = false;
   muted = input<boolean>(false);
 
+  private translate = inject(TranslateService);
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      this.isMobileView = window.innerWidth <= 768;
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (typeof window !== 'undefined') {
+      this.isMobileView = window.innerWidth <= 768;
+    }
+  }
+
+  togglePopover() {
+    this.showPopover = !this.showPopover;
+  }
+
   get nzColor() {
-    return LicensesColors[this.license()];
+    return LicensesColors[this.license];
+  }
+
+  get description() {
+    return this.translate.instant(`LICENSES_DESCRIPTIONS.${this.license}`);
   }
 }
