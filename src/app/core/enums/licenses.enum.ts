bd769b95bf8ce6df8be2872a28994f6d4792c864
adelpro <adelpro@gmail.com>
2025-11-27 13:35:00 +0100

    feat: fxed license labels error related to licence enum naming with hyphens and underscores
    - alignement with the back-end naming with hyphens

--- src/app/core/enums/licenses.enum.ts
+++ src/app/core/enums/licenses.enum.ts
@@ -1,20 +1,20 @@
 export enum Licenses {
   CC0 = 'CC0',
-  CC_BY = 'CC_BY',
-  CC_BY_SA = 'CC_BY_SA',
-  CC_BY_ND = 'CC_BY_ND',
-  CC_BY_NC = 'CC_BY_NC',
-  CC_BY_NC_SA = 'CC_BY_NC_SA',
-  CC_BY_NC_ND = 'CC_BY_NC_ND',
+  CC_BY = 'CC-BY',
+  CC_BY_SA = 'CC-BY-SA',
+  CC_BY_ND = 'CC-BY-ND',
+  CC_BY_NC = 'CC-BY-NC',
+  CC_BY_NC_SA = 'CC-BY-NC-SA',
+  CC_BY_NC_ND = 'CC-BY-NC-ND',
 }
 
 /* eslint-disable @typescript-eslint/no-duplicate-enum-values */
 export enum LicensesColors {
   'CC0' = '#00bc7d',
-  'CC_BY' = '#00bc7d',
-  'CC_BY_SA' = '#fe9a00',
-  'CC_BY_ND' = '#fe9a00',
-  'CC_BY_NC' = '#fe9a00',
-  'CC_BY_NC_SA' = '#fb2c36',
-  'CC_BY_NC_ND' = '#fb2c36',
+  'CC-BY' = '#00bc7d',
+  'CC-BY-SA' = '#fe9a00',
+  'CC-BY-ND' = '#fe9a00',
+  'CC-BY-NC' = '#fe9a00',
+  'CC-BY-NC-SA' = '#fb2c36',
+  'CC-BY-NC-ND' = '#fb2c36',
 }
